e943b5e0f0c4d2f211d9470bb3d14c70a3b5ae5b
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-03 16:04:59 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 03 - MATRICES M.06/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace EXERCISE_03___MATRICES_M._06 {
+    //-----> START
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    class Program {
+        static void Main(string[] args) {
+            string[] line = Console.ReadLine().Split(' '); // -------- > DECLARAÇÃO DO VETOR | Linha de comando para estrever o VETOR com espaço |
+            int m = int.Parse(line[0]); // -------- > Nº de LINHAS da matriz
+            int n = int.Parse(line[1]); // -------- > Nº de COLUNAS da matriz
+
+            int[,] mat = new int[m,n]; // -------- > DECLARAÇÃO da matriz
+
+            for (int i=0; i < m; i++) {
+                string[] values = Console.ReadLine().Split(' '); // -------- > Entrada dos valor POR LINHA 
+                for (int j = 0; j < n; j++) {
+                    mat[i, j] = int.Parse(values[j]); // -------- > Nº de LINHAS da matriz
+                }
+            }
+
+            int x = int.Parse(Console.ReadLine()); // -------- > ENTRADA DO VALOR  a ser pesquisada
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (mat[i, j]== x) {  // -------- >  Parametro da CONDICIONAL
+                        Console.WriteLine("Position " +i+","+j+":");
+                        if (j > 0) {
+                            Console.WriteLine("Left: "+mat[i,j-1]);
+                        }
+                        if (i > 0) {
+                            Console.WriteLine("Up: " +mat[i-1,j]);
+                        }
+                        if(i > n - 1) {
+                            Console.WriteLine("Right: "+mat[i,j+1]);
+                        }
+                        if (i > m - 1) {
+                            Console.WriteLine("Down:"+mat[i+1,j]);
+                        }
+                    }
+                }         
+            }
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //-----> END
+}
